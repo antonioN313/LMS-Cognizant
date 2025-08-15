cee53e357557b527b301c578a280e853eca25be1
antonioN313 <antoniocarlosfilho2003@gmail.com>
2025-08-15 18:48:22 -0300

    DesafioValidarSenha

---
+++ src/main/java/DesafioValidarSenha.java
@@ -0,0 +1,16 @@
+class DesafioValidarSenha {
+  public static void main(String args[]) {
+
+    String senha = args.length > 0 ? args[0] : " ";
+
+    if (senha.length() >=8 && senha.matches(".*\\d.*") && senha.matches(".*[A-Z].*") 
+        && senha.matches(".*[a-z].*") && senha.matches(".*[^a-zA-Z0-9].*")) 
+    {
+      System.out.println("Senha Valida");
+    } else{
+      System.out.println("Senha Invalida");
+    }
+    
+  }
+  
+}
